a155f36a0ca8b0d694900d93b26cdd001345f2d5
knakto <kasichonooo@gmail.com>
2025-06-06 19:07:40 +0700

    fixed: wall as wall

--- srcs/mlx/key_bind.c
+++ srcs/mlx/key_bind.c
@@ -6,7 +6,7 @@
 /*   By: knakto <knakto@student.42bangkok.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 17:34:39 by knakto            #+#    #+#             */
-/*   Updated: 2025/06/06 18:33:03 by knakto           ###   ########.fr       */
+/*   Updated: 2025/06/06 19:07:15 by knakto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,33 @@
 #include "cube.h"
 #include "game.h"
 #include <math.h>
+
+bool	in_wall(float x, float y)
+{
+	// bool	status;
+	int		scale;
+	int		i;
+	int		j;
+
+	// status = false;
+	scale = get_game()->scale;
+	x -= 1;
+	y -= 1;
+	i = 0;
+	printf("%f, %f\n", x, y);
+	while (i < 3)
+	{
+		j = 0;
+		while (j < 3)
+		{
+			if (get_game()->map.map[(int)((x + i) / scale)][(int)((y + j) / scale)].type == WALL)
+				return (false);
+			j++;
+		}
+		i++;
+	}
+	return (true);
+}
 
 void	player_walk(mlx_t *mlx)
 {
@@ -35,13 +62,14 @@ void	player_walk(mlx_t *mlx)
 	{
 		*x1 -= y2 * 3;
 		*y1 += x2 * 3;
-		pnf("%d, %d\n",((int)(*x1 / get_game()->scale) + 1), ((int)(*x1 / get_game()->scale) + 1));
+		pnf("%d, %d\n",((int)(*x1 / get_game()->scale)), ((int)(*y1 / get_game()->scale)));
 		// pnf("%d\n", get_game()->map.map[(int)(*x1 / get_game()->scale) + 1][(int)(*y1 / get_game()->scale) + 1].type);
 		// if ( == WALL)
-		// {
-		// 	*x1 = temp.x;
-		// 	*y1 = temp.y;
-		// }
+		if (in_wall(*x1, *y1))
+		{
+			*x1 = temp.x;
+			*y1 = temp.y;
+		}
 	}
 	else if (mlx_is_key_down(mlx, MLX_KEY_S))
 	{
